990450a5e1db821237b86b6197df8d2ff8f7b087
Marco Antonio <marcotonho38@gmail.com>
2023-05-16 22:02:09 -0300

    refactor : Adicionando get e set na classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -20,4 +20,85 @@ public class Pessoa {
     System.out.println("\nInforme a sua idade: ");
     idade = scanner.nextInt();
   }
+
+
+  public int getIdade() {
+    return idade;
+  }
+
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+
+  public String getCartaoVacinaEmDia() {
+    return cartaoVacinaEmDia;
+  }
+
+
+  public void setCartaoVacinaEmDia(String cartaoVacinaEmDia) {
+    this.cartaoVacinaEmDia = cartaoVacinaEmDia;
+  }
+
+
+  public String getTeveSintomasRecentemente() {
+    return teveSintomasRecentemente;
+  }
+
+
+  public void setTeveSintomasRecentemente(String teveSintomasRecentemente) {
+    this.teveSintomasRecentemente = teveSintomasRecentemente;
+  }
+
+
+  public String getTeveContatoComPessoasSintomaticas() {
+    return teveContatoComPessoasSintomaticas;
+  }
+
+
+  public void setTeveContatoComPessoasSintomaticas(String teveContatoComPessoasSintomaticas) {
+    this.teveContatoComPessoasSintomaticas = teveContatoComPessoasSintomaticas;
+  }
+
+
+  public String getEstaRetornandoViagem() {
+    return estaRetornandoViagem;
+  }
+
+
+  public void setEstaRetornandoViagem(String estaRetornandoViagem) {
+    this.estaRetornandoViagem = estaRetornandoViagem;
+  }
+
+
+  public int getPorcentagemInfeccao() {
+    return porcentagemInfeccao;
+  }
+
+
+  public void setPorcentagemInfeccao(int porcentagemInfeccao) {
+    this.porcentagemInfeccao = porcentagemInfeccao;
+  }
+
+
+  public String getOrientacaoFinal() {
+    return orientacaoFinal;
+  }
+
+
+  public void setOrientacaoFinal(String orientacaoFinal) {
+    this.orientacaoFinal = orientacaoFinal;
+  }
+
+
+  public String getNome() {
+    return nome;
+  }
+
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  
 }
